1491f9780eb3254d606da1bff60e0191fbaa0ebe
Jyot189 <jyotkhant2002@gmail.com>
2023-05-26 16:42:26 +0530

    day-8

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Redirect, Route, RouteProps } from 'react-router-dom';
+import { useAuthContext } from '../context/auth';
 import { RoutePaths } from '../utils/enum';
 
 
 const PrivateRoute = ({ component: Component, path }: RouteProps) => {
-  // if (!authContext.user.id) {
-  //   return <Redirect to={RoutePaths.Login} />;
-  // }
+  const authContext = useAuthContext();
+  if (!authContext.user.id) {
+    return <Redirect to={RoutePaths.Login} />;
+  }
 
   return <Route component={Component} path={path} />;
 };
--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -8,17 +8,22 @@ import siteLogo from "../../assets/images/site-logo.svg";
 import cartIcon from "../../assets/images/cart.png";
 import searchIcon from "../../assets/images/search.png";
 import { TextField, Button } from "@material-ui/core";
+import Shared from "../../utils/shared";
+import { AuthContextModel, useAuthContext } from "../../context/auth";
 import { RoutePaths } from "../../utils/enum";
 import { toast } from "react-toastify";
 import { useHistory } from "react-router-dom";
-// import bookService from "../../service/book.service";
-// import { BookModel } from "../../models/BookModel";
+import bookService from "../../service/book.service";
+import { BookModel } from "../../models/BookModel";
+import { CartContextModel, useCartContext } from "../../context/cart";
 
 const Header: React.FC = () => {
 	const classes = headerStyle();
+	const authContext: AuthContextModel = useAuthContext();
+	const cartContext: CartContextModel = useCartContext();
 	const [open, setOpen] = useState<boolean>(false);
 	const [query, setquery] = useState<string>("");
-	// const [bookList, setbookList] = useState<BookModel[]>([]);
+	const [bookList, setbookList] = useState<BookModel[]>([]);
 	const [openSearchResult, setOpenSearchResult] = useState<boolean>(false);
 	const [loading, setLoading] = useState<boolean>(false);
 
@@ -27,6 +32,52 @@ const Header: React.FC = () => {
 	// for mobile menu
 	const openMenu = (): void => {
 		document.body.classList.toggle("open-menu");
+	};
+
+	const items = useMemo(() => {
+		return Shared.NavigationItems.filter(
+			(item) =>
+				!item.access.length || item.access.includes(authContext.user.roleId)
+		);
+	}, [authContext.user]);
+
+	const logOut = () => {
+		authContext.signOut();
+		cartContext.emptyCart();
+	};
+
+	const getBooks = async () => {
+		const res = await bookService.getAll({
+			pageIndex: 1,
+			pageSize: 10,
+			keyword: query,
+		});
+		setLoading(false);
+		setbookList(res.records);
+	};
+
+	const search = () => {
+		setLoading(true);
+		document.body.classList.add("search-results-open");
+		getBooks();
+		setOpenSearchResult(true);
+	};
+
+	const addToCart = (book: BookModel): void => {
+		if (!authContext.user.id) {
+			toast.error("Please login before adding books to cart");
+			history.push(RoutePaths.Register);
+			return;
+		} else {
+			Shared.addToCart(book, authContext.user.id).then((res) => {
+				if (res.error) {
+					toast.error(res.message);
+				} else {
+					toast.success(res.message);
+					cartContext.updateCart();
+				}
+			});
+		}
 	};
 
 	return (
@@ -47,7 +98,7 @@ const Header: React.FC = () => {
 							<div className="nav-wrapper">
 								<div className="top-right-bar">
 									<List className="top-nav-bar">
-										
+										{!authContext.user.id && (
 											<>
 												<ListItem>
 													<Link to={RoutePaths.Login} title="Login">
@@ -60,21 +111,35 @@ const Header: React.FC = () => {
 													</Link>
 												</ListItem>
 											</>
-										
-										
+										)}
+										{items.map((item, index: number) => (
+											<ListItem key={index}>
+												<Link to={item.route} title={item.name}>
+													{item.name}
+												</Link>
+											</ListItem>
+										))}
 									</List>
 									<List className="cart-country-wrap">
 										<ListItem className="cart-link">
 											<Link to="/cart" title="Cart">
 												<img src={cartIcon} alt="cart.png" />
-												<span></span>
+												<span>{cartContext.cartData.totalRecords}</span>
 												Cart
 											</Link>
 										</ListItem>
 										<ListItem className="hamburger" onClick={openMenu}>
 											<span></span>
 										</ListItem>
-									</List>									
+									</List>
+
+									{authContext.user.id && (
+										<List className="right">
+											<Button onClick={() => logOut()} variant="outlined">
+												Log out
+											</Button>
+										</List>
+									)}
 								</div>
 							</div>
 						</div>
@@ -103,6 +168,47 @@ const Header: React.FC = () => {
 										}
 									/>
 
+									{openSearchResult && (
+										<>
+											<div className="product-listing">
+												{bookList?.length == 0 && !loading && (
+													<p className="no-product">No product found</p>
+												)}
+												{loading ? (
+													<p className="loading">Loading....</p>
+												) : (
+													<List className="related-product-list">
+														{bookList?.length > 0 &&
+															bookList.map((item: BookModel) => {
+																return (
+																	<ListItem>
+																		<div className="inner-block">
+																			<div className="left-col">
+																				<span className="title">
+																					{item.name}
+																				</span>
+																				<p>{item.description}</p>
+																			</div>
+																			<div className="right-col">
+																				<span className="price">
+																					{item.price}
+																				</span>
+																				<Link
+																					to="/"
+																					onClick={() => addToCart(item)}
+																				>
+																					Add to cart
+																				</Link>
+																			</div>
+																		</div>
+																	</ListItem>
+																);
+															})}
+													</List>
+												)}
+											</div>
+										</>
+									)}
 								</div>
 								<Button
 									type="submit"
@@ -110,7 +216,7 @@ const Header: React.FC = () => {
 									variant="contained"
 									color="primary"
 									disableElevation
-									
+									onClick={search}
 								>
 									<em>
 										<img src={searchIcon} alt="search" />
