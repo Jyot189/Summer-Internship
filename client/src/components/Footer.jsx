094e09c784a6a3d02eda1d23610387d369310a4d
Jyot189 <jyotkhant2002@gmail.com>
2023-05-27 15:56:35 +0530

    frontend few changes

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="bg-gray-200 max-w-screen h-36 mx-auto mb-0 px-24 flex items-center justify-center flex-col">
             <div>Logo</div>
-            <div>© 2023 Abhi. All Rights reserved.</div>
+            <div>© 2023 jyot. All Rights reserved.</div>
         </div>
     );
 }
