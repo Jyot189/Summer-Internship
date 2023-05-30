e597a780c3749cdc47672b943179fb23dd00013e
Jyot189 <jyotkhant2002@gmail.com>
2023-05-30 18:23:18 +0530

    day-10

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,10 @@
 export default function Footer() {
     return (
         <div className="bg-gray-200 max-w-screen h-36 mx-auto mb-0 px-24 flex items-center justify-center flex-col">
-            <div>Logo</div>
-            <div>© 2023 Tatvasoft.com. All rights reserved.</div>
+            <div>
+            <img src="http://localhost:5173/site-logo.svg" className="w-48 h-28" />
+            </div>
+            <div>© 2023 jyot khant. All Rights reserved.</div>
         </div>
     );
 }
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -3,3 +3,5 @@ export { default as Login } from "./Login.jsx";
 export { default as PageNotFound } from "./PageNotFound.jsx";
 export {default as Product} from "./Product.jsx";
 export {default as Edit} from "./Edit.jsx";
+export {default as Home} from "./Home.jsx";
+export {default as BookList} from "./BookList.jsx"
